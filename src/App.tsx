3b820bb0715cc5722014ed51ea11160d421154be
Christelle <christouseri@gmail.com>
2025-05-08 19:29:50 -0500

    Adding gradient

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -8,26 +7,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs text-3xl font-bold underline">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="w-screen h-64 bg-gradient-to-t from-rose-300 via-fuschia-400 to-purple-300"></div>
+   
     </>
   )
 }
