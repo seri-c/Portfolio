bde4829e2b092849de45b0e26a1643a4cf44c737
Christelle <christouseri@gmail.com>
2025-05-11 10:46:01 -0500

    Adding components

---
+++ src/components/FooterGradient.tsx
@@ -0,0 +1,10 @@
+
+export default function FooterGradient() {
+
+    return (
+        <div className="w-screen h-90 bg-linear-to-b from-offwhite from-20% to-pink-100">
+
+        </div>
+    );
+
+}
---
+++ src/components/InfoGradient.tsx
@@ -0,0 +1,18 @@
+
+export default function InfoGradient() {
+
+    return (
+        <div className="w-screen overscroll-none flex h-screen bg-linear-to-bl from-rose-300 to-amber-100">
+
+            <div className="text-center text-copper p-4 m-auto font-[Optima]">
+
+                <p className="  font-medium text-6xl">Hi! I'm Christelle Seri</p>
+                <p className=" text-xl font-medium ">A passionate Fullstack Developer with 2 years of experience in building functional and user friendly features for websites.
+                </p>
+
+            </div>
+
+        </div>
+    );
+
+}
