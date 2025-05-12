002e503604f50d44b51b763b328f4150f055619c
Christelle <christouseri@gmail.com>
2025-05-12 11:45:03 -0500

    increased font on resume on mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,13 +24,13 @@ export default function NavBar() {
                 </div>
 
 
-                <a href="#" className="hidden md:block md:text-lg">Home</a>
+                <a href="#" className="hidden md:block md:text-xl">Home</a>
                 <a href="#About" className="hidden md:block md:text-lg">About</a>
                 <a href="#Experience" className="hidden md:block md:text-lg">Experience</a>
                 <a href="#Projects"className="hidden md:block md:text-lg">Projects</a>
 
                 <div>
-                    <a href={ResumePdf} target="_blank"  className="text-xs md:text-lg  shadow-lg shadow-rose-300/20 border border-copper/50 hover:bg-copper hover:text-white hover:cursor-pointer  rounded-lg px-2.5 py-1.5 me-2  ">
+                    <a href={ResumePdf} target="_blank"  className=" md:text-lg  shadow-lg shadow-rose-300/20 border border-copper/50 hover:bg-copper hover:text-white hover:cursor-pointer  rounded-lg px-2.5 py-1.5 me-2  ">
                         Resume
                     </a>
                 </div>
